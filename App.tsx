2df2ffe03ee822dc7e0b076f0a74538ca2dfdb0b
Mikwuegbu <mstephen130@yahoo.com>
2025-01-28 16:34:08 +0100

    Add FavouriteScreen and integrate it into the navigation drawer

--- App.tsx
+++ App.tsx
@@ -3,14 +3,63 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer, useRoute } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import MealsOverViewScreen, {
-	RouteParams,
-} from './screens/MealsOverViewScreen';
+import MealsOverViewScreen from './screens/MealsOverViewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import FavouriteScreen from './screens/FavouriteScreen';
+import { Ionicons } from '@expo/vector-icons';
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+	return (
+		<Drawer.Navigator
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: '#351401',
+				},
+				drawerItemStyle: {
+					borderRadius: 10,
+					marginVertical: 6,
+				},
+				headerTintColor: '#fff',
+				sceneStyle: {
+					backgroundColor: '#3f2f25',
+				},
+				drawerContentStyle: {
+					backgroundColor: '#3f2f25',
+				},
+				drawerInactiveTintColor: 'white',
+				drawerActiveTintColor: '#351401',
+				drawerActiveBackgroundColor: '#e4baa1',
+			}}
+		>
+			<Drawer.Screen
+				name="Categories"
+				component={CategoriesScreen}
+				options={{
+					title: 'All Categories',
+					drawerIcon: ({ color, size }) => (
+						<Ionicons name="list" size={size} color={color} />
+					),
+				}}
+			/>
+			<Drawer.Screen
+				name="favorites"
+				component={FavouriteScreen}
+				options={{
+					title: 'All Categories',
+					drawerIcon: ({ color, size }) => (
+						<Ionicons name="star" size={size} color={color} />
+					),
+				}}
+			/>
+		</Drawer.Navigator>
+	);
+};
 
 export default function App() {
-	const Stack = createNativeStackNavigator();
-
 	return (
 		<>
 			<StatusBar style="light" />
@@ -30,9 +79,10 @@ export default function App() {
 					<Stack.Screen
 						options={{
 							title: 'All Categories',
+							headerShown: false,
 						}}
 						name="MealsCategories"
-						component={CategoriesScreen}
+						component={DrawerNavigator}
 					></Stack.Screen>
 					<Stack.Screen
 						options={{
@@ -51,10 +101,7 @@ export default function App() {
 					<Stack.Screen
 						name="mealDetail"
 						options={{
-							title: 'Meal Detail',
-							// headerRight: () => {
-							// 	return <Button title="Tap me!" />;
-							// },
+							title: 'About the Meal',
 						}}
 						component={MealDetailScreen}
 					/>
---
+++ screens/FavouriteScreen.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+const FavouriteScreen = () => {
+	return <Text>The favorites screen</Text>;
+};
+
+export default FavouriteScreen;
